c01a51dd192e3ee5fc845ab718b78a2fc51d19c4
rehabmahmoud20 <rehabgendy16@gmail.com>
2022-04-10 10:03:11 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(".navbar #mainnav ul li a").click(function(){
+    $('body,html').animate({scrollTop:$("#" + $(this).data('value')).offset().top},1000)
+})
+  
+//quotes
+let quotes = [
+    {quote:"“Be yourself; everyone else is already taken.”" ,author:"Oscar Wilde"},
+    {quote:"“So many books, so little time.”" ,author:" Frank Zappa"},
+    {quote:"“A room without books is like a body without a soul.”" ,author:" Marcus Tullius Cicero"},
+    {quote:"“You only live once, but if you do it right, once is enough.”" ,author:"Mae West"},
+    {quote:"“Be the change that you wish to see in the world.”" ,author:" Mahatma Gandhi"}
+]
+function getQuote(){
+let rondomQuote = Math.floor(Math.random()*quotes.length)
+document.getElementById("quo").innerHTML =`${quotes[rondomQuote].quote}` 
+document.getElementById("author").innerHTML = `${quotes[rondomQuote].author}`
+
+}
